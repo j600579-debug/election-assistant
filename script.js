02c6c830ee86d70493c6b5a6c2838d549cd34ddb
j600579-debug <j600579@gmail.com>
2026-04-28 00:58:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,31 @@
 function show(id){
-  document.getElementById("quiz").style.display="none";
-  document.getElementById("chat").style.display="none";
-  document.getElementById(id).style.display="block";
+    document.querySelectorAll('.section').forEach(s=>s.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
 }
 
-/* QUIZ */
-
-let questions = [
-  {
-    q:"Who conducts elections in India?",
-    a:["Supreme Court","ECI","Parliament"],
-    c:1
-  },
-  {
-    q:"Minimum voting age?",
-    a:["16","18","21"],
-    c:1
-  },
-  {
-    q:"What is EVM?",
-    a:["Machine","Paper","Law"],
-    c:0
-  }
-];
-
-let i=0, score=0;
-
-function load(){
-  document.getElementById("q").innerText = questions[i].q;
-  document.getElementById("a0").innerText = questions[i].a[0];
-  document.getElementById("a1").innerText = questions[i].a[1];
-  document.getElementById("a2").innerText = questions[i].a[2];
+function flip(el){
+    if(el.innerText=="ECI") el.innerText="Election Commission of India";
+    else if(el.innerText=="EVM") el.innerText="Voting Machine";
+    else if(el.innerText=="VVPAT") el.innerText="Vote Slip System";
+    else if(el.innerText=="NOTA") el.innerText="None Of The Above";
 }
 
-window.onload = load;
-
-function answer(x){
-  if(x == questions[i].c){
-    score++;
-  }
-
-  i++;
-
-  if(i < questions.length){
-    load();
-  } else {
-    document.getElementById("q").innerText = "Finished!";
-    document.getElementById("score").innerText = "Score: " + score;
-  }
+function check(ans){
+    if(ans=="b"){
+        document.getElementById("result").innerText="Correct!";
+    }else{
+        document.getElementById("result").innerText="Wrong!";
+    }
 }
-
-/* CHAT */
 
 function send(){
-  let input = document.getElementById("input").value;
-  let box = document.getElementById("chatbox");
+    let text=document.getElementById("msg").value;
+    let reply="";
 
-  let reply = "";
+    if(text.includes("vote")) reply="Voting happens at polling booth";
+    else if(text.includes("age")) reply="You must be 18+";
+    else reply="Ask about vote, age, process";
 
-  if(input.includes("vote")){
-    reply = "Go to booth and vote.";
-  }
-  else if(input.includes("age")){
-    reply = "Minimum age is 18.";
-  }
-  else{
-    reply = "Ask about vote or age.";
-  }
-
-  box.innerHTML += "<p>You: "+input+"</p>";
-  box.innerHTML += "<p>Bot: "+reply+"</p>";
+    document.getElementById("chatArea").innerText=
+    "You: "+text+"\nBot: "+reply;
 }
